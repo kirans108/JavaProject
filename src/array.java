b14603d6a9db88e49867894ed04fd60015f9a237
Seemah26 <48144991+Seemah26@users.noreply.github.com>
2022-06-20 15:34:13 +0530

    20 june coding update

---
+++ src/array.java
@@ -0,0 +1,11 @@
+
+public class array {
+	
+		public static void main (String [] arg) {
+			int[] a= {1,2,3,4,2,7,8,8,3};
+			System.out.println("array elements are:");
+			for (int i=0; i<a.length;i++)
+			System.out.println(a[i]);
+		}
+}
+
