92df0ee3153bb167531c75f8ce11c54e0898623a
ADMIN <ADMIN@DESKTOP-DKDQVD8>
2022-06-20 17:44:48 +0530

    arrays

--- src/array.java
+++ src/array.java
@@ -3,7 +3,9 @@ public class array {
 	
 		public static void main (String [] arg) {
 			int[] a= {1,2,3,4,2,7,8,8,3};
+			int b[]= {2,3,5,86};
 			System.out.println("array elements are:");
+			System.out.println("a+b");
 			for (int i=0; i<a.length;i++)
 			System.out.println(a[i]);
 		}
